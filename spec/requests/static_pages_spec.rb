95900fda0555c0651abb67ea6076391f41196f23
kelea <kelea98@gmail.com>
2013-03-18 22:01:16 +0100

    Added new pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,4 +7,25 @@ describe "Static pages" do
       page.should have_content('Template App')
       end
   end
+
+  describe "Help page" do
+  	it "should have the content 'Help'" do
+  		visit '/static_pages/help'
+  		page.should have_content('Help')
+  		end
+  	end
+  
+  describe "About Us" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+      end
+    end
+
+  describe "Contact Us" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact Us')
+      end
+  end
 end
